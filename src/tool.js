e333da57dbb29f656efedc88a075692ba34b128d
BHess2653 <GM_Dahaka@BHMac.local>
2018-01-03 23:20:34 -0500

    made first two cases text and password

--- src/tool.js
+++ src/tool.js
@@ -2,6 +2,21 @@ exports.formGenerator = (formInputs) => {
   let output = '';
 
   formInputs.forEach((input) => {
-    
+    if (input.type) {
+      switch (input.type.toLowerCase()) {
+
+        case 'text':
+          output += `<input type="${input.type}" `;
+          output += input.id ? `id="${input.id}" ` : '';
+          output += input.class ? `class="${input.class}" ` : '';
+          output += input.placeholder ? `placeholder="${input.placeholder}" ` : '';
+          output += input.required ? 'required' : '';
+          output += '/>';
+
+        break;
+
+        case 'password':
+      }
+    }
   })
 };
