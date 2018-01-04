579b6f70ac49e7e3afbe1de0878723d0e3d8856a
BHess2653 <GM_Dahaka@BHMac.local>
2018-01-03 23:27:14 -0500

    made two more cases label and submit

--- src/tool.js
+++ src/tool.js
@@ -8,14 +8,27 @@ exports.formGenerator = (formInputs) => {
         case 'text':
           output += `<input type="${input.type}" `;
           output += input.id ? `id="${input.id}" ` : '';
-          output += input.class ? `class="${input.class}" ` : '';
           output += input.placeholder ? `placeholder="${input.placeholder}" ` : '';
           output += input.required ? 'required' : '';
           output += '/>';
-
         break;
 
         case 'password':
+
+        case 'label':
+          output += '<label ';
+          output += input.id ? `id="${input.id}" ` : '';
+          output += input.for ? `for="${input.for}" ` : '';
+          output += input.text ? `>${input.text}</label>` : '></label>';
+        break;
+
+        case 'submit':
+          output += `<input type="${input.type}" `;
+          output += input.id ? `id="${input.id}" ` : '';
+          output += input.name ? `name="${input.name}" ` : '';
+          output += input.value ? `value="${input.value}" ` : '';
+          output += '></input>';
+        break;
       }
     }
   })
