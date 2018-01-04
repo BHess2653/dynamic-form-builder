b759efe6def9d9b87ea346f27f32be4482be9106
BHess2653 <GM_Dahaka@BHMac.local>
2018-01-03 22:48:58 -0500

    created main.js with inputArry working on tool.js

---
+++ src/main.js
@@ -0,0 +1,38 @@
+const tool = require('./tool.js');
+
+const inputArry = [
+  {
+    type: 'label',
+    for: 'email',
+    text: 'E-mail',
+    id: 'email',
+  },
+  {
+    type: 'text',
+    name: 'email',
+    id: 'email',
+    placeholder: 'E-mail address...',
+    required: true,
+  },
+  {
+    type: 'label',
+    for: 'password',
+    text: 'Password',
+    id: 'password',
+  },
+  {
+    type: 'password',
+    name: 'password',
+    id: 'password',
+    placeholder: 'Password...',
+    required: true,
+  },
+  {
+    type: 'submit',
+    name: 'submit',
+    id: 'submit',
+    value: 'Submit',
+  },
+];
+
+console.log(tool.Build(inputArry));
