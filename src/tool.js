b7a338a7c16025693eb05596b10cb6014cfe17d8
BHess2653 <GM_Dahaka@BHMac.local>
2018-01-06 18:23:15 -0500

    fixed and corrected errors in tool.js

--- src/tool.js
+++ src/tool.js
@@ -4,8 +4,13 @@ exports.formGenerator = (formInputs) => {
   formInputs.forEach((input) => {
     if (input.type) {
       switch (input.type.toLowerCase()) {
-
         case 'password':
+          output += `<input type="${input.type}" `;
+          output += input.id ? `id="${input.id}" ` : '';
+          output += input.placeholder ? `placeholder="${input.placeholder}" ` : '';
+          output += input.required ? 'required' : '';
+          output += '/>';
+          break;
 
         case 'text':
           output += `<input type="${input.type}" `;
@@ -13,14 +18,14 @@ exports.formGenerator = (formInputs) => {
           output += input.placeholder ? `placeholder="${input.placeholder}" ` : '';
           output += input.required ? 'required' : '';
           output += '/>';
-        break;
+          break;
 
         case 'label':
           output += '<label ';
           output += input.id ? `id="${input.id}" ` : '';
           output += input.for ? `for="${input.for}" ` : '';
           output += input.text ? `>${input.text}</label>` : '></label>';
-        break;
+          break;
 
         case 'submit':
           output += `<input type="${input.type}" `;
@@ -28,7 +33,7 @@ exports.formGenerator = (formInputs) => {
           output += input.name ? `name="${input.name}" ` : '';
           output += input.value ? `value="${input.value}" ` : '';
           output += '></input>';
-        break;
+          break;
 
         default:
           throw new Error(`❗️ Error❗️ invalid type ➡️  ${input.type} ⬅️`);
@@ -36,6 +41,6 @@ exports.formGenerator = (formInputs) => {
     } else {
       throw new Error('❗️ Error❗️ Missing input type please Check input array');
     }
-  })
+  });
   return `<form method="post">${output}</form>`;
 };
