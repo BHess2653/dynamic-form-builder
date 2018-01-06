c73e767d26d6c901f38fec60794d5fe2a8182523
BHess2653 <GM_Dahaka@BHMac.local>
2018-01-06 18:47:39 -0500

    updated and added comments tool.js

--- src/tool.js
+++ src/tool.js
@@ -1,9 +1,13 @@
 exports.formGenerator = (formInputs) => {
   let output = '';
 
+  // loop through array
   formInputs.forEach((input) => {
+    // checks for element of type
     if (input.type) {
+      // switch to check input type
       switch (input.type.toLowerCase()) {
+        // check if form has id, text, name, etc.
         case 'password':
           output += `<input type="${input.type}" `;
           output += input.id ? `id="${input.id}" ` : '';
@@ -36,11 +40,13 @@ exports.formGenerator = (formInputs) => {
           break;
 
         default:
+          // error if input type doesnt match
           throw new Error(`❗️ Error❗️ invalid type ➡️  ${input.type} ⬅️`);
       }
     } else {
       throw new Error('❗️ Error❗️ Missing input type please Check input array');
     }
   });
+  // return Output
   return `<form method="post">${output}</form>`;
 };
